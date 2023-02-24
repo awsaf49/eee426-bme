31733caff6909436e12b15a3f8dd6fb27e81fb8d
Awsaf <awsaf49@gmail.com>
2023-02-25 01:13:51 +0600

    email-15mins | tweet-5mins

--- src/thingspeak-analysis.m
+++ src/thingspeak-analysis.m
@@ -15,15 +15,23 @@ options = weboptions("HeaderFields", ["ThingSpeak-Alerts-API-Key", alertApiKey ]
 
 % Set the email subject.
 alertSubject = sprintf("Health Report");
-
-% Read the recent data.
-NumDays = 2;
+NumDays = 10;
 ESP_Ts = 15; % esp time between two values
-Alert_Ts = 15; % thingspeak time between two runs
-NumValues = 1; % Alert_Ts*60/ESP_Ts;
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%
+%%%%%% Tweet %%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%
+% Read the recent data.
+Alert_Ts = 5; % thingspeak time between two runs
+NumValues = Alert_Ts*60/ESP_Ts;
+disp(NumValues);
 
 Fields = [1, 2]; % bpm (ppg)
 data = thingSpeakRead(channelID, NumDays=NumDays, Fields=Fields, ReadKey='DLRZA3TM5QHDZ4G6');
+% NumDays=NumDays, NumPoints=NumValues, 
 
 bpm_ppg  = data(:, 1);
 spo2_ppg = data(:, 2);
@@ -34,37 +42,54 @@ if isempty(bpm_ppg) & isempty(spo2_ppg)
 else
     % Get the values of last 15mins
     bpm_ppg = bpm_ppg(length(bpm_ppg)-NumValues+1:end);
-    spo2_ppg = spo2_ppg(length(bpm_ppg)-NumValues+1:end);
+    spo2_ppg = spo2_ppg(length(spo2_ppg)-NumValues+1:end);
+%     display(spo2_ppg)
+%     
+%     display(bpm_ppg)
     
     % Get the most recent point in the array.
 %     bbm_ppg_last = bpm_ppg(end);
 
     % Get max value of the 15min
-    bpm_ppg_value = max(bpm_ppg); 
-    spo2_ppg_value = max(spo2_ppg); 
+    bpm_ppg_max = max(bpm_ppg); 
+    bpm_ppg_min = min(bpm_ppg);
+    
+    spo2_ppg_max = max(spo2_ppg); 
+    spo2_ppg_min = min(spo2_ppg);
     
     % Get current time
     time = datestr(datetime('now'));
     
     % Set the bpm message
-    if (bpm_ppg_value<60)
+    % abnormality either Brady or Tachy
+    if (bpm_ppg_min<60)
         bpm_ppg_msg = "Bradycardia";
-    elseif (bpm_ppg_value>100)
+        bpm_ppg_value = bpm_ppg_min;
+    elseif (bpm_ppg_max>100)
         bpm_ppg_msg = "Tachycardia";
+        bpm_ppg_value = bpm_ppg_max;
     else
         bpm_ppg_msg = "Normal"; % sprintf(" Normal. (BPM: %d) (Time: %s)", bpm_ppg_value, time);
+        bpm_ppg_value = bpm_ppg(end);
     end
+%     disp(bpm_ppg_min);
     
     % Set the spo2 message
-    if (spo2_ppg_value<85)
+    if (spo2_ppg_min<85)
         spo2_ppg_msg = "Severe Hypoxia";
-    elseif (bpm_ppg_value<90)
+        spo2_ppg_value = spo2_ppg_min;
+    elseif (spo2_ppg_min<90)
         spo2_ppg_msg = "Moderate Hypoxia";
-    elseif (bpm_ppg_value<95)
+        spo2_ppg_value = spo2_ppg_min;
+    elseif (spo2_ppg_min<95)
         spo2_ppg_msg = "Mild Hypoxia";
+        spo2_ppg_value = spo2_ppg_min;
     else
         spo2_ppg_msg = "Normal"; % sprintf(" Normal. (BPM: %d) (Time: %s)", bpm_ppg_value, time);
+        spo2_ppg_value = spo2_ppg(end);
     end
+    % set spo2 value to min
+    
     
     % get formatted message with bpm and spo2
     alertBody = sprintf(" BPM: %d (%s!) | SpO2: %d (%s!) | (Time: %s)", bpm_ppg_value, bpm_ppg_msg, spo2_ppg_value, spo2_ppg_msg, time);
@@ -72,7 +97,6 @@ end
 
 
 if ~strcmp(bpm_ppg_msg, "Normal") | ~strcmp(spo2_ppg_msg, "Normal")
-        
     % Send tweet
     try
         webwrite('https://api.thingspeak.com/apps/thingtweet/1/statuses/update', 'api_key', 'TOM4CYDR6JIVVMYZ', 'status', alertBody);
@@ -80,7 +104,89 @@ if ~strcmp(bpm_ppg_msg, "Normal") | ~strcmp(spo2_ppg_msg, "Normal")
     catch someException
         fprintf("[Tweet] Failed to send alert: %s\n", someException.message);
     end
-     
+else
+    fprintf(alertBody);
+end
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%
+%%%%%% Email %%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%
+% Read the recent data.
+Alert_Ts = 15; % thingspeak time between two runs
+NumValues = Alert_Ts*60/ESP_Ts;
+disp(NumValues);
+
+Fields = [1, 2]; % bpm (ppg)
+data = thingSpeakRead(channelID, NumDays=NumDays, Fields=Fields, ReadKey='DLRZA3TM5QHDZ4G6');
+% NumDays=NumDays, NumPoints=NumValues, 
+
+bpm_ppg  = data(:, 1);
+spo2_ppg = data(:, 2);
+
+% Check to make sure the data was read correctly from the channel.
+if isempty(bpm_ppg) & isempty(spo2_ppg)
+    alertBody = ' Not Active. ';
+else
+    % Get the values of last 15mins
+    bpm_ppg = bpm_ppg(length(bpm_ppg)-NumValues+1:end);
+    spo2_ppg = spo2_ppg(length(spo2_ppg)-NumValues+1:end);
+%     display(spo2_ppg)
+%     
+%     display(bpm_ppg)
+    
+    % Get the most recent point in the array.
+%     bbm_ppg_last = bpm_ppg(end);
+
+    % Get max value of the 15min
+    bpm_ppg_max = max(bpm_ppg); 
+    bpm_ppg_min = min(bpm_ppg);
+    
+    spo2_ppg_max = max(spo2_ppg); 
+    spo2_ppg_min = min(spo2_ppg);
+    
+    % Get current time
+    time = datestr(datetime('now'));
+    
+    % Set the bpm message
+    % abnormality either Brady or Tachy
+    if (bpm_ppg_min<60)
+        bpm_ppg_msg = "Bradycardia";
+        bpm_ppg_value = bpm_ppg_min;
+    elseif (bpm_ppg_max>100)
+        bpm_ppg_msg = "Tachycardia";
+        bpm_ppg_value = bpm_ppg_max;
+    else
+        bpm_ppg_msg = "Normal"; % sprintf(" Normal. (BPM: %d) (Time: %s)", bpm_ppg_value, time);
+        bpm_ppg_value = bpm_ppg(end);
+    end
+%     disp(bpm_ppg_min);
+    
+    % Set the spo2 message
+    if (spo2_ppg_min<85)
+        spo2_ppg_msg = "Severe Hypoxia";
+        spo2_ppg_value = spo2_ppg_min;
+    elseif (spo2_ppg_min<90)
+        spo2_ppg_msg = "Moderate Hypoxia";
+        spo2_ppg_value = spo2_ppg_min;
+    elseif (spo2_ppg_min<95)
+        spo2_ppg_msg = "Mild Hypoxia";
+        spo2_ppg_value = spo2_ppg_min;
+    else
+        spo2_ppg_msg = "Normal"; % sprintf(" Normal. (BPM: %d) (Time: %s)", bpm_ppg_value, time);
+        spo2_ppg_value = spo2_ppg(end);
+    end
+    % set spo2 value to min
+    
+    
+    % get formatted message with bpm and spo2
+    alertBody = sprintf(" BPM: %d (%s!) | SpO2: %d (%s!) | (Time: %s)", bpm_ppg_value, bpm_ppg_msg, spo2_ppg_value, spo2_ppg_msg, time);
+end
+
+
+if ~strcmp(bpm_ppg_msg, "Normal") | ~strcmp(spo2_ppg_msg, "Normal")
      % Catch errors so the MATLAB code does not disable a TimeControl if it fails
     try
         webwrite(alertUrl , "body", alertBody, "subject", alertSubject, options);
@@ -91,4 +197,3 @@ if ~strcmp(bpm_ppg_msg, "Normal") | ~strcmp(spo2_ppg_msg, "Normal")
 else
     fprintf(alertBody);
 end
-    
